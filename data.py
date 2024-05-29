a110fe12868e7285fb20e0dd0e812e9e54ba3ca9
Steve Ng <steve.ng@offbeattech.com>
2024-05-29 15:07:06 +0800

    init

---
+++ data.py
@@ -0,0 +1,120 @@
+[
+    {
+        "storeId": "483",
+        "storeName": "Japanholic",
+        "country": "JP",
+        "avatar": "https://file-user-profile-asset.everuts.com/sto-img/e426a661-0243-47de-87a5-3045e4eb2230.webp",
+        "currency": "JPY"
+    },
+    {
+        "storeId": "1086",
+        "storeName": "姸媽韓國雜貨店",
+        "country": "KR",
+        "avatar": "https://file-user-profile-asset.everuts.com/sto-img/a27dd250-77cf-4ea8-972f-84cc0d90d079.webp",
+        "currency": "KRW"
+    },
+    {
+        "storeId": "4319",
+        "storeName": "TT泰國代購",
+        "country": "TH",
+        "avatar": "https://file-user-profile-asset.everuts.com/sto-img/85a86ec1-f3f7-4b9d-8be3-878b8cd4da52.webp",
+        "currency": "THB"
+    },
+    {
+        "storeId": "441",
+        "storeName": "Coco Hon-LoveToBuy喜歡買",
+        "country": "TW",
+        "avatar": "https://file-user-profile-asset.everuts.com/sto-img/2fc6ce97-8e59-4891-b2fe-a326dd2a8646.webp",
+        "currency": "TWD"
+    },
+    {
+        "storeId": "6396",
+        "storeName": "Manhattan",
+        "country": "JP",
+        "avatar": "https://file-user-profile-asset.everuts.com/sto-img/2cceaa20-bd7e-411e-8fa5-9e7cef2f822b.webp",
+        "currency": "JPY"
+    }
+]
+
+data_products = [
+    {
+        "productImage": "https://product-assets.everuts.com/store/7424/7bb77bc-screenshot2024-05-27at6.00.47pm.png",
+        "productName": "Ottogi 大邱辣牛肉湯 500g X 3",
+        "category": "E060000000000",
+        "categories_en": {
+            "lvl0": "Food & Drink"
+        },
+        "categories_zhhk": {
+            "lvl0": "食品及飲料"
+        },
+        # "sellingPrice": 10000,
+        # "originalPrice": null,
+        "weight": 1
+    },
+    {
+        "productImage": "https://product-assets.everuts.com/store/7424/2bafd3a-screenshot2024-05-27at5.58.56pm.png",
+        "productName": "CJ 牛蒡營養飯 200g X 3",
+        "category": "E060000000000",
+        "categories_en": {
+            "lvl0": "Food & Drink"
+        },
+        "categories_zhhk": {
+            "lvl0": "食品及飲料"
+        },
+        # "sellingPrice": 10000,
+        # "originalPrice": null,
+        "weight": 1
+    },
+    {
+        "productImage": "https://product-assets.everuts.com/store/4986/7772a6a-2024-05-2512.53.14.png",
+        "productName": "ETVOS礦物UV身體防曬碎粉 SPF40 PA+++【8克】",
+        "category": "E030010010001",
+        "categories_en": {
+            "lvl0": "Beauty & Fragrance",
+            "lvl1": "Beauty & Fragrance > Skincare",
+            "lvl2": "Beauty & Fragrance > Skincare > Cream"
+        },
+        "categories_zhhk": {
+            "lvl0": "美妝及香水",
+            "lvl1": "美妝及香水 > 護膚品",
+            "lvl2": "美妝及香水 > 護膚品 > 面霜"
+        },
+        # "sellingPrice": 10000,
+        # "originalPrice": null,
+        "weight": 1
+    },
+    {
+        "productImage": "https://product-assets.everuts.com/store/5852/c53f8c0-5copy.jpeg",
+        "productName": "YOU+MORE! 阿拉斯加雪橇犬的寶寶抱枕",
+        "category": "E130050000000",
+        "sellingPrice": 10000,
+        "weight": 1,
+        "categories_en": {
+            "lvl0": "Hobbies",
+            "lvl1": "Hobbies > Toys & Figures"
+        },
+        "categories_zhhk": {
+            "lvl0": "興趣及愛好",
+            "lvl1": "興趣及愛好 > 玩具及模型"
+        },
+        # "originalPrice": null,
+    },
+    {
+        "productImage": "https://product-assets.everuts.com/store/5627/bb81dde-h-lohacoxu03001i20240514183444.jpeg",
+        "productName": "[數量限定]集中修護髮膜 10g x 8個",
+        "category": "E030030040001",
+        "sellingPrice": 10000,
+        "weight": 1,
+        "categories_en": {
+            "lvl0": "Beauty & Fragrance",
+            "lvl1": "Beauty & Fragrance > Hair",
+            "lvl2": "Beauty & Fragrance > Hair > Hair Mask"
+        },
+        "categories_zhhk": {
+            "lvl0": "美妝及香水",
+            "lvl1": "美妝及香水 > 頭髮",
+            "lvl2": "美妝及香水 > 頭髮 > 髮膜"
+        },
+        # "originalPrice": null,
+    },
+]
